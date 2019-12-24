2e380978b77463e03443e664f47f3f1c2af7ba69
Shannon Ervin <sjervin07@gmail.com>
2019-12-23 20:55:48 -0600

    linked github api,  constructed json for user data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const axios = require("axios");
 
 
 
@@ -16,3 +17,40 @@ function init() {
 console.log("hello")
 }
 init();
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}`;
+
+    axios.get(queryUrl).then(function(res) {
+      const user =  {
+          avatar_url: res.data.avatar_url,
+          login: res.data.login,
+          bio: res.data.bio,
+          public_repos: res.data.public_repos,
+          followers: res.data.followers,
+          following: res.data.following,
+
+        
+      };
+      console.log(user.avatar_url)
+      console.log(user.login)
+      console.log(user.bio)
+      console.log(user.public_repos)
+      console.log(user.followers)
+      console.log(user.following)
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
+  });
